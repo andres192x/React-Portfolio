fb5cf4b5fe6a534fb2df6fc82725807160f64abf
andres192x <andres192x@hotmail.com>
2023-02-04 14:00:15 -0500

    added boilerplate code

---
+++ portfolio/src/components/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <section id='about'>
+            <div className="container">
+                <h1>lol</h1>
+            </div>
+
+        </section>
+        
+
+
+    );
+}
+
